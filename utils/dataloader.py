29e0ab00745060bfdb082ec12e06b8754142ecab
tina-1007 <yiting1777@gmail.com>
2021-11-01 12:05:16 +0800

    update

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -16,7 +16,7 @@ def get_loader(args):
     train_transform=transforms.Compose([transforms.Resize((600, 600), Image.BILINEAR),
                                 transforms.RandomCrop((448, 448)),
                                 transforms.RandomHorizontalFlip(),
-                                transforms.RandomRotation(30),
+                                # transforms.RandomRotation(30),
                                 transforms.ToTensor(),
                                 transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
     val_transform=transforms.Compose([transforms.Resize((600, 600), Image.BILINEAR),
